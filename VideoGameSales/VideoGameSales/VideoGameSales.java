9556e6f0e0159dc3e57ec70a082f367c8f418397
AzureQDragon <darthmaul0310@gmail.com>
2020-02-27 21:31:44 -0500

    Fixed Classes

---
+++ VideoGameSales/VideoGameSales/VideoGameSales.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VideoGameSales;
+import java.util.ArrayList;
+import core.data.*;
+
+/**
+ *
+ * @author jfhs-lab3021-22
+ */
+public class VideoGameSales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DataSource ds = DataSource.connect("Video_Game_Sales.csv").load();
+
+        ArrayList<VideoGames> sales = ds.fetchList("VideoGames", "VideoGames/name", "VideoGames/platform", "VideoGames/yearOfRelease", "VideoGames/genre", "VideoGames/publisher", "VideoGames/naSales", "VideoGames/euSales", "VideoGames/jpSales", "VideoGames/otherSales", "VideoGames/globalSales", "VideoGames/criticScore", "VideoGames/criticCount", "VideoGames/userScore", "VideoGames/userCount", "VideoGames/developer", "VideoGames/rating");
+        System.out.println(sales.get(1));
+    }
+    
+}
