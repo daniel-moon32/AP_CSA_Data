9e04ed0963cb5ca62aec78b669f31ca80e116808
AzureQDragon <36148173+AzureQDragon@users.noreply.github.com>
2020-02-28 13:31:37 -0500

    Fixed java class

--- VideoGameSales/VideoGameSales/VideoGames.java
+++ VideoGameSales/VideoGameSales/VideoGames.java
@@ -11,47 +11,51 @@ package VideoGameSales;
  */
 public class VideoGames {
 
-    /**
-     * @param args the command line arguments
-     *
-     */
-    private String name;
-    private String platform;
-    private int yearOfRelease;
-    private String genre;
-    private String publisher;
-    private double naSales;
-    private double euSales;
-    private double jpSales;
-    private double otherSales;
-    private double globalSales;
-    private int criticScore;
-    private int criticCount;
-    private int userScore;
-    private int userCount;
-    private String developer;
-    private int rating;
-    
-    VideoGames(String name, String platform, int yearOfRelease, String genre, String publisher, double naSales, double euSales, double jpSales, double globalSales, int criticScore, int criticCount, int userScore, int userCount, String developer, int rating)
-    {
-        this.name = name;
-        this.platform = platform;
-        this.yearOfRelease = yearOfRelease;
-        this.genre = genre;
-        this.publisher = publisher;
-        this.naSales = naSales;
-        this.euSales = euSales;
-        this.jpSales = jpSales;
-        this.otherSales = otherSales;
-        this.globalSales = globalSales;
-        this.criticScore = criticScore;
-        this.criticCount = criticCount;
-        this.userScore = userScore;
-        this.userCount = userCount;
-        this.developer = developer;
-        this.rating = rating;
+        /**
+         * @param args the command line arguments
+         *
+         */
+        private String name;
+        private String platform;
+        private int yearOfRelease;
+        private String genre;
+        private String publisher;
+        private double naSales;
+        private double euSales;
+        private double jpSales;
+        private double otherSales;
+        private double globalSales;
+        private int criticScore;
+        private int criticCount;
+        private double userScore;
+        private int userCount;
+        private String developer;
+        private String rating;
+
+        VideoGames(String name, String platform, int yearOfRelease, String genre, String publisher, double naSales, double euSales, double jpSales, double otherSales, double globalSales, int criticScore, int criticCount, double userScore, int userCount, String developer, String rating)
+        {
+            this.name = name;
+            this.platform = platform;
+            this.yearOfRelease = yearOfRelease;
+            this.genre = genre;
+            this.publisher = publisher;
+            this.naSales = naSales;
+            this.euSales = euSales;
+            this.jpSales = jpSales;
+            this.otherSales = otherSales;
+            this.globalSales = globalSales;
+            this.criticScore = criticScore;
+            this.criticCount = criticCount;
+            this.userScore = userScore;
+            this.userCount = userCount;
+            this.developer = developer;
+            this.rating = rating;
+        }
+        
+        
+
+
+
     }
-    
-    
-    
-}
+
+
